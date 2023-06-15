d3d03ac2916b2c692d2858ad66a9cac145db5379
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-06-15 08:50:17 -0300

    Estruturação de pastas; Use-case de carrinho.

--- test/calculosFolhaDePagamento.spec.js
+++ test/calculosFolhaDePagamento.spec.js
@@ -1,15 +1,17 @@
-import { calculaDescontos, somaHorasExtras } from "../calculosFolhaDePagamento";
+import { calculaDescontos, somaHorasExtras } from "../src/folha-pagamento/calculosFolhaDePagamento";
 
-test('deve retornar a soma das horas extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('testes dos calculos de folha', () => {
+  it('deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
-});
+    expect(retornado).toBe(esperado);
+  });
 
-test('deve descontar valor no salario', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
+  it('deve descontar valor no salario', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
